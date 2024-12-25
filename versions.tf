7efafd629eb68600e7160480380096dd11628c1d
Nikhilsethiya <niksethiya003@gmail.com>
2024-12-25 13:13:54 +0530

    changes for random string

--- versions.tf
+++ versions.tf
@@ -6,13 +6,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.75.0" # Latest major version for AzureRM provider
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0" # Latest version for Random provider
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.3.0" # Latest version for Null provider
-    }
   }
 }
